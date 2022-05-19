29bf9f0edbe0d0ce0f18ab26899a4ade72cde799
Aristotel1990 <aristotel.dhrako@gmail.com>
2022-05-19 13:50:40 +0200

    faturarrr

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,143 +1,211 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import { useStateValue } from "../StateProvider";
 
 function Header() {
-    const [{ basket}, dispatch] = useStateValue();
-    const chill='https://www.youtube.com/watch?v=36YnV9STBqc&ab_channel=TheGoodLifeRadioxSensualMusique';
-    const deep='https://www.youtube.com/watch?v=WsDyRAPFBC8&ab_channel=MonstercatSilk';
-    const rock='https://www.youtube.com/watch?v=nwYAt3-IPHY&ab_channel=RGNMusic';
-    const Reggaeton='https://www.youtube.com/watch?v=DteFDJlPpHg&ab_channel=LewisBennett';
-    const trans='https://www.youtube.com/watch?v=WYetg3AuLE4&ab_channel=RazNitzanMusic';
-    const classic='https://www.youtube.com/watch?v=p6JCAB2E0to&ab_channel=YellowBrickCinema-RelaxingMusic'
-    const meditation ='https://www.youtube.com/watch?v=g44sfv5KGwo&ab_channel=YellowBrickCinema-RelaxingMusic'
-    const jazz='https://www.youtube.com/watch?v=Dx5qFachd3A&ab_channel=CafeMusicBGMchannel'
-    const alternative='https://www.youtube.com/watch?v=5X18D-EbjUc&ab_channel=frequenzy'
-    const tropical ='https://www.youtube.com/watch?v=SIt21jdTYKk&ab_channel=BallisticLive'
-    const rain='https://www.youtube.com/watch?v=R0NME9W3cR4&t=80s&ab_channel=NatureSFX'
-    const ocean='https://www.youtube.com/watch?v=vPhg6sc1Mk4&ab_channel=NaturalezaViva-SonidosyPaisajesIncre%C3%ADbles'
-    const river='https://www.youtube.com/watch?v=IvjMgVS6kng&t=8s&ab_channel=TheSilentWatcher'
-    const hits = 'https://www.youtube.com/watch?v=o2ASc0qPK8M&ab_channel=BestofMix'
-    const oriental= 'https://www.youtube.com/watch?v=niv4jMBX6yA'
-    return (
-        
-            <Nav>
-                  <NavMenu>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:chill,
-                        anime:"/images/chill1.gif"})
-                    }}>
-                      <span>Lounge Chillout </span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:deep,
-                        anime:"/images/deep.gif"})
-                    }}>
-                      <span>Deep House</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:hits,
-                        anime:"/images/hit1.gif"})
-                    }}>
-                      <span>Hits</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:tropical,
-                        anime:"/images/giphy.gif",
-                      })
-                    }}>
-                      <span>Tropical</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:rock,
-                        anime:"/images/rock1.gif"
-                      })
-                    }}>
-                      <span>Rock</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:alternative,
-                        anime:"/images/rock.gif"})
-                    }}>
-                      <span>Alternative</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:Reggaeton,
-                        anime:"/images/rega.gif"})
-                    }}>
-                      <span>Reggaeton</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:oriental,
-                        anime:"/images/oriental.gif"
-                      })
-                    }}>
-                      <span>Oriental</span>
-                    </a>
-                  
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:jazz,
-                        anime:"/images/jazz.gif"})
-                    }}>
-                      <span>Relaxing Jazz</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:trans,
-                        anime:"/images/trance.gif"})
-                    }}>
-                      <span>Trance</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:classic,
-                        anime:"/images/clasic.gif"})
-                    }}>
-                      <span>Classical Instrumental</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:meditation,
-                        anime:"/images/meditation.gif"})
-                    }}>
-                      <span>Meditation</span>
-                    </a>
-                  
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:rain,
-                        anime:"/images/rain.gif"})
-                    }}>
-                      <span>Rain</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:ocean,
-                        anime:"/images/ocean.gif"})
-                    }}>
-                      <span>Ocean</span>
-                    </a>
-                    <a onClick={()=>{
-                        dispatch({ type:'ADD_TO_BASKET',
-                        item:river,
-                        anime:"/images/river.gif"})
-                    }}>
-                      <span>River</span>
-                    </a>
-                  </NavMenu>
-               
-            </Nav>
-        
-    )
+  const [{ basket }, dispatch] = useStateValue();
+  const chill =
+    "https://www.youtube.com/watch?v=36YnV9STBqc&ab_channel=TheGoodLifeRadioxSensualMusique";
+  const deep =
+    "https://www.youtube.com/watch?v=WsDyRAPFBC8&ab_channel=MonstercatSilk";
+  const rock = "https://www.youtube.com/watch?v=U2wfGCojp3w&ab_channel=RGN";
+  const Reggaeton =
+    "https://www.youtube.com/watch?v=TL-fkiYXj2Q&ab_channel=MusicaBachata";
+  const trans =
+    "https://www.youtube.com/watch?v=WYetg3AuLE4&ab_channel=RazNitzanMusic";
+  const classic =
+    "https://www.youtube.com/watch?v=2gO1v2GPMFk&ab_channel=ILoveYouVenice";
+  const meditation =
+    "https://www.youtube.com/watch?v=g44sfv5KGwo&ab_channel=YellowBrickCinema-RelaxingMusic";
+  const jazz =
+    "https://www.youtube.com/watch?v=Dx5qFachd3A&ab_channel=CafeMusicBGMchannel";
+  const alternative =
+    "https://www.youtube.com/watch?v=5X18D-EbjUc&ab_channel=frequenzy";
+  const tropical =
+    "https://www.youtube.com/watch?v=SIt21jdTYKk&ab_channel=BallisticLive";
+  const rain =
+    "https://www.youtube.com/watch?v=R0NME9W3cR4&t=80s&ab_channel=NatureSFX";
+  const ocean =
+    "https://www.youtube.com/watch?v=vPhg6sc1Mk4&ab_channel=NaturalezaViva-SonidosyPaisajesIncre%C3%ADbles";
+  const river =
+    "https://www.youtube.com/watch?v=IvjMgVS6kng&t=8s&ab_channel=TheSilentWatcher";
+  const hits =
+    "https://www.youtube.com/watch?v=19hKXI1ENrY&ab_channel=TheGoodLifeRadioxSensualMusique";
+  const oriental =
+    "https://www.youtube.com/watch?v=DAbHQzGiDMo&ab_channel=CafeDeAnatoliaLOUNGE";
+  return (
+    <Nav>
+      <NavMenu>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: chill,
+              anime: "/images/chill1.gif",
+            });
+          }}
+        >
+          <span>Lounge Chillout </span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: deep,
+              anime: "/images/deep.gif",
+            });
+          }}
+        >
+          <span>Deep House</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: hits,
+              anime: "/images/hit1.gif",
+            });
+          }}
+        >
+          <span>Hits</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: tropical,
+              anime: "/images/giphy.gif",
+            });
+          }}
+        >
+          <span>Tropical</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: rock,
+              anime: "/images/rock1.gif",
+            });
+          }}
+        >
+          <span>Rock</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: alternative,
+              anime: "/images/rock.gif",
+            });
+          }}
+        >
+          <span>Alternative</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: Reggaeton,
+              anime: "/images/rega.gif",
+            });
+          }}
+        >
+          <span>Reggaeton</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: oriental,
+              anime: "/images/oriental.gif",
+            });
+          }}
+        >
+          <span>Oriental</span>
+        </a>
+
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: jazz,
+              anime: "/images/jazz.gif",
+            });
+          }}
+        >
+          <span>Relaxing Jazz</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: trans,
+              anime: "/images/trance.gif",
+            });
+          }}
+        >
+          <span>Trance</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: classic,
+              anime: "/images/clasic.gif",
+            });
+          }}
+        >
+          <span>Classical Instrumental</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: meditation,
+              anime: "/images/meditation.gif",
+            });
+          }}
+        >
+          <span>Meditation</span>
+        </a>
+
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: rain,
+              anime: "/images/rain.gif",
+            });
+          }}
+        >
+          <span>Rain</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: ocean,
+              anime: "/images/ocean.gif",
+            });
+          }}
+        >
+          <span>Ocean</span>
+        </a>
+        <a
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_BASKET",
+              item: river,
+              anime: "/images/river.gif",
+            });
+          }}
+        >
+          <span>River</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
 }
 const Nav = styled.nav`
   position: fixed;
@@ -205,12 +273,12 @@ const NavMenu = styled.div`
   }
   @media (max-width: 768px) {
     display: flex;
-    position:relative;
-    white-space:nowrap;
+    position: relative;
+    white-space: nowrap;
     overflow-x: auto;
-    justify-content:flex-start;
+    justify-content: flex-start;
     margin-right: auto;
-  margin-left: auto;
+    margin-left: auto;
   }
 `;
-export default Header
+export default Header;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,116 +1,130 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
-import ReactPlayer from 'react-player/lazy';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ReactPlayer from "react-player/lazy";
 import { useStateValue } from "../StateProvider";
 
 function Login() {
-    const [{ basket,gif}, dispatch] = useStateValue();
+  const [{ basket, gif }] = useStateValue();
 
-   const [state, setstate] = useState(false)
-   const [data, setData] = useState('Listen to Radio Dhermi')
-   const [dis, setDis] = useState('none')
-   const [back, setBack] = useState('')
-   const [logo, setLogo] = useState('')
- useEffect(() => {
-    const onChange=()=>{
-        if(state===true){
-            setData('Stop')
-            setDis('')
-            setBack('')
-            setLogo('none')
-        }else{
-            setData('Listen to Radio Dhermi')
-            setDis('none')
-            setBack('/images/drymades.jpg')
-            setLogo('')
+  const [state, setstate] = useState(false);
+  const [data, setData] = useState("Listen to Radio Dhermi");
+  const [dis, setDis] = useState("none");
+  const [back, setBack] = useState("");
+  const [logo, setLogo] = useState("");
+  const [kusht, setKusht] = useState(null);
 
-        }
-    }
-    onChange()
- }, [state])
-    return (
-       <Container>
-           <Content>
-               <CTO>
-               <CTOGif style={{display:`${dis}`}} src={gif} alt=""/>
-                <CTOLogo style={{display:`${logo}`}}  src="/images/logo.png" alt=""/>
+  useEffect(() => {
+    const onChange = () => {
+      if (state === true) {
+        setData("Stop");
+        setDis("");
+        setBack("");
+        setLogo("none");
+        setKusht(5);
+      } else {
+        setData("Listen to Radio Dhermi");
+        setDis("none");
+        setBack("/images/drymades.jpg");
+        setLogo("");
+        setKusht(null);
+      }
+    };
+    onChange();
+  }, [state]);
+  return (
+    <Container>
+      <Content>
+        <CTO>
+          <CTOGif style={{ display: `${dis}` }} src={gif} alt="" />
+          <CTOLogo
+            style={{ display: `${logo}` }}
+            src="/images/logo.png"
+            alt=""
+          />
 
-                   <Button  onClick={()=>{
-                       
-                       setstate(!state)
-                       setDis('')
-                   }}> {data} </Button>
-                   <ReactPlayer playing={state} style={{display:'none'}} url={basket} />
-               </CTO>
-               <BgImage style={{backgroundImage:`url(${back})`}}/>
-           </Content>
-       </Container>
-    )
-   
+          <Button
+            onClick={() => {
+              setstate(!state);
+              setDis("");
+            }}
+          >
+            {" "}
+            {data}{" "}
+          </Button>
+          <ReactPlayer
+            playing={state}
+            kusht
+            style={{ display: !kusht ? "none" : "block" }}
+            url={basket}
+          />
+        </CTO>
+        <BgImage style={{ backgroundImage: `url(${back})` }} />
+      </Content>
+    </Container>
+  );
 }
 const Container = styled.section`
-overflow: hidden;
-display:flex;
-flex-direction:column;
-text-align:center;
-height:100vh;
-`
-const Content=styled.div`
-margin-bottom:10vh;
-width:100%;
-position: relative;
-min-height:100vh;
-box-sizing:border-box;
-display:flex;
-justify-content:center;
-align-items:center;
-flex-direction:column;
-padding:80px 40px;
-height:100%;
-`
-const BgImage =styled.div`
-height:100%;
-background-position:top;
-background-size:cover;
-background-repeat:no-repeat;
-position: absolute;
-background-color:black;
-right:0;
-left:0;
-z-index:-1;
-`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+const Content = styled.div`
+  margin-bottom: 10vh;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  background-color: black;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
 const CTO = styled.div`
- margin-bottom:2vw;
- max-width:650px;
-display:flex;
-flex-wrap:wrap;
-justify-content:center;
-flex-direction:column;
-margin-top:0;
-align-items:center;
-text-align:center;
-margin-right:auto;
-margin-left:auto;
-
-`
-const CTOLogo=styled.img`
-margin-bottom:12px;
-max-width:600px;
-min-height:1px;
-display:block;
-width:100%;
-`
-const CTOGif=styled.img`
-margin-bottom:12px;
-max-width:600px;
-min-height:1px;
-display:block;
-width:100%;
-`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+`;
+const CTOLogo = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+const CTOGif = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
 
 const Button = styled.button`
-font-weight: bold;
-cursor: pointer;
+  font-weight: bold;
+  cursor: pointer;
   color: #f9f9f9;
   background-color: black;
   margin-bottom: 12px;
@@ -122,8 +136,8 @@ cursor: pointer;
   border-radius: 4px;
   &:hover {
     background-color: white;
-    color:black
+    color: black;
   }
-`
+`;
 
-export default Login
+export default Login;
